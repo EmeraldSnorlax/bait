94ff91faa6ad4d111c5f0fd1f5dc15df940611b6
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-25 17:14:03 +0000

    setup jest

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,8 @@ import express from 'express';
 import frontend from './frontend/frontend';
 import api from './api/api';
 
-const PORT = 5500;
 const app = express();
+export default app;
 
 app.use('/', frontend);
 app.use('/api', api);
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import app from './app';
+
+const PORT = 5500;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
