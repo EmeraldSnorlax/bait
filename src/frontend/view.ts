bd75e47b1670655dac2b4a2b08ee0a349f53739f
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-25 21:48:10 +0000

    good night

--- src/frontend/view.ts
+++ src/frontend/view.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const view = express.Router();
 export default view;
 
+// TODO: This. Also, I'm not sure if this passes the test?
 view.get('/:id', (req, res) => {
   res.send('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 });
