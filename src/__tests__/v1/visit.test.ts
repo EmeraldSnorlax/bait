7a6e591a326d371d9699ea71c3b4cadf8a1dab5b
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-25 19:58:09 +0000

    use example data in the test

--- src/__tests__/v1/visit.test.ts
+++ src/__tests__/v1/visit.test.ts
@@ -1,5 +1,7 @@
 import supertest from 'supertest';
+import Link from '../../api/v1/link/link';
 import app from '../../app';
+import db from '../../db';
 
 describe('Visit a link', () => {
   it('should error on invalid links', async () => {
@@ -8,4 +10,28 @@ describe('Visit a link', () => {
     expect(res.status).toEqual(404);
     expect(res.body).toContain('not exist');
   });
+  it('should redirect valid links', async () => {
+    await db.run('CREATE TABLE IF NOT EXISTS links (id STRING PRIMARY KEY, title STRING, description STRING, image STRING, color STRING, destination STRING);');
+    const post: Link = {
+      content: {
+        title: 'Bill Gates to buy FSF for $4.20bn',
+        description: 'The retired multi-billionaire is embracing open-source.',
+        color: '#f9a825',
+      },
+      destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    };
+
+    let res = await supertest(app)
+      .post('/api/v1/create')
+      .send(post);
+    const id = res.body.id!;
+    res = await supertest(app)
+      .get(`/${id}`);
+    expect(res.body).toContain('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  });
+});
+
+afterAll((done) => {
+  db.close();
+  done();
 });
