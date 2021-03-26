d48920e7e5b550d9139c88762851b900a548e15e
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-26 16:48:10 +0000

    refcator tests

---
+++ src/__tests__/__mocks__/testLink.ts
@@ -0,0 +1,12 @@
+import Link from '../../api/v1/link/link';
+
+const link: Link = {
+  content: {
+    title: 'Bill Gates to buy FSF for $4.20bn',
+    description: 'The retired multi-billionaire is embracing open-source.',
+    color: '#f9a825',
+  },
+  destination: 'https://youtube.com/watch?v=dQw4w9WgXcQ',
+};
+
+export default link;
