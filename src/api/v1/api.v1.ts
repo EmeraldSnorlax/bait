b617f0a2fcf7ce166777a5870d61cb1d37e6c0e0
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-26 17:46:14 +0000

    List sites endpoint (#6)
    * redirects now work
    
    * maybe lets not accidentally clear this file
    
    * endpoint to retrieve allowed sites info

--- src/api/v1/api.v1.ts
+++ src/api/v1/api.v1.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import allowedSites from './allowedSites';
 import create from './link/create';
 
 const apiV1 = express.Router();
 export default apiV1;
 
 apiV1.use('/create', create);
+apiV1.use('/allowedSites', allowedSites);
