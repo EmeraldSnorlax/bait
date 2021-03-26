d4e884be8427cc11ac7c87fc1e9cd3ec12e9bab5
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-26 16:53:25 +0000

    reject not allowed links

--- src/__tests__/__mocks__/testLink.ts
+++ src/__tests__/__mocks__/testLink.ts
@@ -9,4 +9,13 @@ const link: Link = {
   destination: 'https://youtube.com/watch?v=dQw4w9WgXcQ',
 };
 
+export const notAllowedLink: Link = {
+  content: {
+    title: 'Bill Gates to buy FSF for $4.20bn',
+    description: 'The retired multi-billionaire is embracing open-source.',
+    color: '#f9a825',
+  },
+  destination: 'https://this-domain-is-not-allowed',
+};
+
 export default link;
--- src/api/v1/link/create.ts
+++ src/api/v1/link/create.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import db from '../../../db';
 import Link from './link';
+import { domains } from '../../../allowList';
 
 const create = express.Router();
 export default create;
@@ -10,26 +11,37 @@ create.use(express.json());
 
 create.post('/', (req, res) => {
   const link: Link = req.body;
-  if (link.destination !== undefined && link.content.title !== undefined) {
-    // Make the fields blank if they don't exist
-    link.content.description = link.content.description || '';
-    link.content.color = link.content.color || '';
-    link.content.image = link.content.image || '';
-    const id = uuidv4();
-    db.run(
-      'INSERT INTO links(id, title, description, image, color, destination) VALUES(?, ?, ?, ?, ?, ?)',
-      [id, link.content.title, link.content.description, link.content.image, link.content.color,
-        link.destination],
-      (err) => {
-        if (err) {
-          res.status(500).send('something went wrong saving this link!');
-          console.log(err);
-        } else {
-          res.status(201).send({ id });
-        }
-      },
-    );
-  } else {
+  try {
+  // Isolate just the domain
+    let domain = req.body.destination.split('/').slice(0, 3);
+    domain[1] = '//';
+    domain = domain.join('');
+    domain += '/';
+    if (link.destination !== undefined && link.content.title !== undefined) {
+      if (!domains.includes(domain)) {
+        res.status(403).send('this link is not allowed!');
+      } else {
+        // Make the fields blank if they don't exist
+        link.content.description = link.content.description || '';
+        link.content.color = link.content.color || '';
+        link.content.image = link.content.image || '';
+        const id = uuidv4();
+        db.run(
+          'INSERT INTO links(id, title, description, image, color, destination) VALUES(?, ?, ?, ?, ?, ?)',
+          [id, link.content.title, link.content.description, link.content.image, link.content.color,
+            link.destination],
+          (err) => {
+            if (err) {
+              res.status(500).send('something went wrong saving this link!');
+              console.log(err);
+            } else {
+              res.status(201).send({ id });
+            }
+          },
+        );
+      }
+    }
+  } catch {
     res.status(400).send('check your post body?');
   }
 });
