4682b877b361681a6ea0ca542f0ce79acac681a9
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-27 17:39:14 +0000

    actually validate things + correct user input

--- src/public/js/allowedSites.js
+++ src/public/js/allowedSites.js
@@ -4,6 +4,34 @@ var destination = document.getElementById('message-box');
 var siteIconEl = document.getElementById('site-icon');
 var allowedSiteList = '';
 
+function validate() {
+	// Validate the link when focus is lost
+	destination.value = destination.value.replace('www.', '').replace('http://', 'https://');
+	if (!destination.value.startsWith('https://')) { destination.value = `https://${destination.value}`; }
+	if (destination.value.charAt(destination.value.length - 1) !== '/') { destination.value += '/'; }
+	let siteName;
+	let siteIcon;
+	let valid = false;
+	sites.forEach((site) => {
+		site.domains.forEach((domain) => {
+			if (destination.value.startsWith(domain)) {
+				siteName = site.name;
+				siteIcon = site.icon;
+				valid = true;
+			}
+		});
+	});
+			
+	// Style the Icon
+	if (!valid) { 
+		siteIconEl.src = './icons/error.svg'
+		siteIconEl.style = 'filter: invert(0.4) sepia(1) saturate(7) hue-rotate(320deg);';
+	} else {
+		siteIconEl.src = siteIcon;
+		siteIconEl.style = 'filter: invert(1);';
+	}
+}
+
 window.onload = () => {
 	fetch(window.location.href + 'api/v1/allowedSites')
 	.then(res => res.json())
@@ -12,31 +40,7 @@ window.onload = () => {
 		sites.forEach((site) => {
 			allowedSiteList += site.name + ', '
 		});
-
-		destination.addEventListener('focusout', () => {
-			// Validate the link when focus is lost
-			destination.value = destination.value.replace('www.', '');
-			let siteName;
-			let siteIcon;
-			let valid = false;
-			sites.forEach((site) => {
-				site.domains.forEach((domain) => {
-					if (destination.value.startsWith(domain)) {
-						siteName = site.name;
-						siteIcon = site.icon;
-						valid = true;
-					}
-				});
-			});
-			
-			// Style the Icon
-			if (!valid) { 
-				siteIconEl.src = './icons/error.svg'
-				siteIconEl.style = 'filter: invert(0.4) sepia(1) saturate(7) hue-rotate(320deg);';
-			 } else {
-				siteIconEl.src = siteIcon;
-				siteIconEl.style = 'filter: invert(1);';
-			 }
-		});		
+		destination.addEventListener('focusout', validate);
+		destination.addEventListener('keyup', (e) => { if (e.key === 'Enter') { validate(); } })
 	});
 }
--- src/public/js/postLink.js
+++ src/public/js/postLink.js
@@ -3,23 +3,29 @@ var description = document.getElementById('description');
 var destination = document.getElementById('message-box');
 
 function post() {
-	const link = {
-		content: {
-			title: title.value,
-			description: description.value,
-			image: imageLink.value,
-			color: colorPicker.value,
-		},
-		destination: destination.value.replace('www.', ''),
-	};
 	// Validate:
+	let allowed = false;
+	sites.forEach((site) => {
+		site.domains.forEach((domain) => {
+			if (destination.value.startsWith(domain)) { allowed = true }
+		})
+	})
 	if (!title.value) {
 		alert('The title can\'t be empty!');
 	} else if (!destination.value) {
 		alert('The destination can\'t be empty!');
-	} else if (!valid) {
+	} else if (!allowed) {
 		alert('That site isn\'t allowed!');
 	} else {
+		const link = {
+			content: {
+				title: title.value,
+				description: description.value,
+				image: imageLink.value,
+				color: colorPicker.value,
+			},
+			destination: destination.value
+		};
 		fetch(window.location.href + 'api/v1/create', {
 			method: 'POST',
 			body: JSON.stringify(link),
